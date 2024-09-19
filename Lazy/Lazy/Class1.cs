40ebad5445b3a077eb14f0d690f96ec0fcbd140d
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2024-09-19 17:05:10 +0300

    feat: create dotnet project
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Lazy/Lazy/Class1.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="Class1.cs" company="Sarapulov Vasilii">
+// Copyright (c) Sarapulov Vasilii. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// https://github.com/Sarapulov-Vas/Homework-3semester/blob/main/LICENSE
+// </copyright>
+
+namespace Lazy;
+
+public class Class1
+{
+
+}
