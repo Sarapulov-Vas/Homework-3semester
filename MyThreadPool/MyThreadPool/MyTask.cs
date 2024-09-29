de360b34c20465c140859117ae648033d2055cb8
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2024-09-29 22:28:13 +0300

    feat: implement the thread pool class
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ MyThreadPool/MyThreadPool/MyTask.cs
@@ -0,0 +1,102 @@
+// <copyright file="MyTask.cs" company="Sarapulov Vasilii">
+// Copyright (c) Sarapulov Vasilii. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// https://github.com/Sarapulov-Vas/Homework-3semester/blob/main/LICENSE
+// </copyright>
+
+using MyThreadPool;
+
+/// <summary>
+/// Class that implements the task interface.
+/// </summary>
+/// <typeparam name="TResult">The type of result of the task calculation.</typeparam>
+public class MyTask<TResult> : IMyTask<TResult>
+{
+    private readonly Func<TResult> function;
+    private readonly object lockObject = new ();
+    private readonly Queue<Action> taskQueue;
+    private readonly CancellationToken cancellation;
+    private Exception? exception;
+    private TResult? result;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyTask{TResult}"/> class.
+    /// </summary>
+    /// <param name="func">Task.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <param name="taskQueue">Task queue.</param>
+    public MyTask(Func<TResult> func, CancellationToken cancellationToken, Queue<Action> taskQueue)
+    {
+        function = func;
+        cancellation = cancellationToken;
+        this.taskQueue = taskQueue;
+        lock (taskQueue)
+        {
+            this.taskQueue.Enqueue(Task);
+            Monitor.Pulse(taskQueue);
+        }
+    }
+
+    /// <inheritdoc/>
+    public bool IsCompleted { get; private set; }
+
+    /// <inheritdoc/>
+    public TResult Result => GetResult();
+
+    /// <inheritdoc/>
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func)
+        => new MyTask<TNewResult>(() => func(Result), cancellation, taskQueue);
+
+    private void Task()
+    {
+        lock (lockObject)
+        {
+            try
+            {
+                if (cancellation.IsCancellationRequested)
+                {
+                    Monitor.PulseAll(lockObject);
+                    return;
+                }
+
+                result = function();
+            }
+            catch (Exception e)
+            {
+                exception = e;
+                IsCompleted = true;
+            }
+
+            Monitor.PulseAll(lockObject);
+        }
+    }
+
+    private TResult GetResult()
+    {
+        lock (lockObject)
+        {
+            while (!IsCompleted && exception is not null)
+            {
+                if (cancellation.IsCancellationRequested)
+                {
+                    throw new TaskCanceledException();
+                }
+
+                Monitor.Wait(lockObject);
+            }
+
+            if (exception is not null)
+            {
+                throw new AggregateException(exception);
+            }
+            else if (result is null)
+            {
+                throw new ArgumentNullException("The function result was null.");
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
---
+++ MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="MyThreadPool.cs" company="Sarapulov Vasilii">
+// Copyright (c) Sarapulov Vasilii. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// https://github.com/Sarapulov-Vas/Homework-3semester/blob/main/LICENSE
+// </copyright>
+
+namespace MyThreadPool;
+
+/// <summary>
+/// Class that implements thread pool.
+/// </summary>
+public class MyThreadPool
+{
+    private readonly CancellationTokenSource cancellation = new ();
+
+    private readonly Queue<Action> taskQueue = new ();
+
+    private readonly Thread[] threads;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyThreadPool"/> class.
+    /// </summary>
+    /// <param name="numberOfThreads">Number of threads.</param>
+    public MyThreadPool(int numberOfThreads)
+    {
+        if (numberOfThreads <= 0)
+        {
+            throw new ArgumentOutOfRangeException("The number of streams must be positive.");
+        }
+
+        threads = new Thread[numberOfThreads];
+        for (int i = 0; i < numberOfThreads; i++)
+        {
+            threads[i] = new Thread(СompleteTask);
+            threads[i].Start();
+        }
+    }
+
+    /// <summary>
+    /// Method to add a task to the thread pool.
+    /// </summary>
+    /// <typeparam name="TResult">Task result type.</typeparam>
+    /// <param name="func">Function to calculate.</param>
+    /// <returns>Task result.</returns>
+    public IMyTask<TResult> Submit<TResult>(Func<TResult> func)
+        => new MyTask<TResult>(func, cancellation.Token, taskQueue);
+
+    /// <summary>
+    /// The method that closes threads.
+    /// </summary>
+    public void Shutdown()
+    {
+        cancellation.Cancel();
+        lock (taskQueue)
+        {
+            Monitor.PulseAll(taskQueue);
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+    }
+
+    private void СompleteTask()
+    {
+        while (!cancellation.IsCancellationRequested || taskQueue.Count > 0)
+        {
+            Action task;
+            lock (taskQueue)
+            {
+                while (taskQueue.Count == 0)
+                {
+                    Monitor.Wait(taskQueue);
+                }
+
+                task = taskQueue.Dequeue();
+            }
+
+            task();
+        }
+    }
+}
