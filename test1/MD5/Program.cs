9031e1dd4867a389c81670efd3e57e9475232816
Sarapulov Vasilii <145163665+Sarapulov-Vas@users.noreply.github.com>
2024-11-11 13:22:14 +0300

    merge: test1 -> main
    * feat: add solution
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: fix codacy issues
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: try to fix tests in windows ci
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: fix paths in program
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: fix test coverage in CI
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: fix test coverage in CI
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: issues
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: fix issues in tests
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: try to fix windows CI
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>
    
    * fix: fix tests
    
    ---------
    
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ test1/MD5/Program.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Program.cs" company="Sarapulov Vasilii">
+// Copyright (c) Sarapulov Vasilii. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// https://github.com/Sarapulov-Vas/Homework-3semester/blob/main/LICENSE
+// </copyright>
+
+using System.Diagnostics;
+using MD5Checksum;
+
+var singleThreadChecksum = new SingleThreadChecksum();
+var multiThreadChecksum = new MultiThreadChecksum();
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+await singleThreadChecksum.CalculateCheckSum("../CheckSum.Tests/TestFiles");
+stopwatch.Stop();
+Console.WriteLine($"Singlethread time(ms): {stopwatch.ElapsedMilliseconds}");
+stopwatch.Reset();
+stopwatch.Start();
+await multiThreadChecksum.CalculateCheckSum("../CheckSum.Tests/TestFiles");
+Console.WriteLine($"Multithread time(ms): {stopwatch.ElapsedMilliseconds}");
---
+++ test1/MD5/SingleThreadChecksum.cs
@@ -0,0 +1,60 @@
+// <copyright file="SingleThreadChecksum.cs" company="Sarapulov Vasilii">
+// Copyright (c) Sarapulov Vasilii. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// https://github.com/Sarapulov-Vas/Homework-3semester/blob/main/LICENSE
+// </copyright>
+
+namespace MD5Checksum;
+
+using System.Security.Cryptography;
+using System.Text;
+
+/// <summary>
+/// A class of single-threaded checksum calculation.
+/// </summary>
+public class SingleThreadChecksum : ICheckSum
+{
+    /// <inheritdoc/>
+    public Task<byte[]> CalculateCheckSum(string path)
+    {
+        if (File.Exists(path))
+        {
+            return FileChecksum(path);
+        }
+        else if (Directory.Exists(path))
+        {
+            return DirectoryChecksum(path);
+        }
+        else
+        {
+            throw new ArgumentException("File/Directory does not exist");
+        }
+    }
+
+    private static Task<byte[]> FileChecksum(string path) =>
+        Task.Run(() => MD5.HashData(new FileStream(path, FileMode.Open, FileAccess.Read)));
+
+    private Task<byte[]> DirectoryChecksum(string path)
+    {
+        var pathName = Path.GetDirectoryName(path);
+        var internalFilesCheksum = Encoding.ASCII.GetBytes(pathName is not null ? pathName : string.Empty);
+        var paths = Directory.GetFileSystemEntries(path);
+        if (paths is not null)
+        {
+            Array.Sort(paths);
+            foreach (var directoryPath in paths)
+            {
+                if (File.Exists(directoryPath))
+                {
+                    internalFilesCheksum.Concat(FileChecksum(directoryPath).Result);
+                }
+                else
+                {
+                    internalFilesCheksum.Concat(DirectoryChecksum(directoryPath).Result);
+                }
+            }
+        }
+
+        return Task.Run(() => MD5.HashData(internalFilesCheksum));
+    }
+}
